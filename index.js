a8e80af5397cfeee64f5b4361b36e965994d65d1
Willian <williansilvap25@gmail.com>
2022-06-25 20:22:26 -0300

    setup of routes and controller

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/routes")
+
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Servidor rodando em: http://localhost:${port}`);
