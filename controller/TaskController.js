23a3e7108f3b8c8b4042c9a27de4bf337d513755
Willian <williansilvap25@gmail.com>
2022-06-27 22:25:36 -0300

    add task getById

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
     try {
         const tasksList = await Task.find();
-        return res.render("index", {tasksList});
+        return res.render("index", { tasksList });
     } catch (err) {
         res.status(500).send({ error: err.message });
     }
@@ -22,7 +22,14 @@ const createTask = async (req, res) => {
     }
 };
 
+const getById = async (req, res) => {
+    const task = await Task.findOne({ _id: req.params.id });
+    const tasksList = await Task.find();
+    res.render("index", { task, tasksList });
+};
+
 module.exports = {
     getAllTasks,
-    createTask
+    createTask,
+    getById
 };
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,6 @@ const TaskController = require("../controller/TaskController");
 
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
+routes.get("/getById", TaskController.getById);
 
 module.exports = routes;
