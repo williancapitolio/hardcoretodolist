95c3f24a8dc74bb68d7c67504501509aa44efb47
Willian <williansilvap25@gmail.com>
2022-06-27 21:37:10 -0300

    task create ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
-const routes = require("./routes/routes")
+const routes = require("./routes/routes");
 const connectToDb = require("./database/db");
 
 connectToDb();
